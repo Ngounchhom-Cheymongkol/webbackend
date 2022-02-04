43e8312fe7a4ad05403078360433ccf4bd3c164c
Ngounchhomn-Cheymongkol <ngounchhom.cheymongol2509@gmail.com>
2022-02-05 01:29:43 +0700

    commit3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
